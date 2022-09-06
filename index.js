7319c7b4a85f4e430f25484c6701a4ae7f775356
Doug-Cal <112691904+Doug-Cal@users.noreply.github.com>
2022-09-06 07:08:45 +0200

    debut js

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ function test (test){
     console.log(test);
 }
 test(test);
+
+const btn1 : getElementbyId(cadreexterne) ==> {
+    //ajouter ici les actions alimentant le CSS
+}
+
+const clic.addEventListener("mousedown") ==> {
+ //ajouter ici la manipulation de btn1
+}
