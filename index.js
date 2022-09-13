19281980934df3f5e7889ab8624cf79cbb577bb1
Doug-Cal <112691904+Doug-Cal@users.noreply.github.com>
2022-09-13 09:46:20 +0200

    go

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-let test = 1;
-function test (test){
-    console.log(test);
-}
-test(test);
+let userName ="001";
+
+const alert = (testUserName) => {
+ if(testUserName == '001'){
+    return `Hello ${userName}`;
+ } else return "unknown";
+};
+
 
 const topBar = document.querySelector("nav");
 
-const test = topBar.addEventListener("mousemove");
+const test = topBar.addEventListener("mouseover") => {
+ alert();
+};
