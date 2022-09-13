60efa9b0ac5e74535d1bcba7504416b2af47c923
Doug-Cal <112691904+Doug-Cal@users.noreply.github.com>
2022-09-13 12:31:07 +0200

    misc

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-const btn1 = document.querySelector("#btn1");
+const buttonBehaviour = document.querySelectorAll(".headerButton");
+buttonBehaviour.addEventListener("click", () => {
+console.log("menu tous boutons");
+});
+
+
+const btn1 = document.getElementById("#btn1");
 btn1.addEventListener("click", () => {
     btn1.classList.add("buttonMouseOver");
-    btn1.style.padding = "0px";
     console.log("click1");
     });
 
 
-const btn2 = document.querySelector("#btn2");
+
+const btn2 = document.getElementbyId("#btn2");
 btn2.addEventListener("click",() => {
     console.log("click2");
-    const square = div.querySelector(#secondZone);
+    const square = div.getElementbyId(#secondZone);
     console.log(square);
     square.createElement("div", id="tile");
     const tile = square.querySelector(#tile);
     console.log(tile);
     tile.add(<p>tuile ici</p>);
-});
+    });
 
