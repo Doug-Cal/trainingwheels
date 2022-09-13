4784ac6d5898a6ab1c8b76eae7112d480f6dae4f
Doug-Cal <112691904+Doug-Cal@users.noreply.github.com>
2022-09-13 12:12:57 +0200

    fix call buttons

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const btn1 = document.querySelector(button);
-
+const btn1 = document.querySelector(#btn1);
 btn1.addEventListener("click", () => {
     btn1.classList.add("buttonMouseOver");
     btn1.style.padding = "0px";
@@ -7,11 +6,12 @@ btn1.addEventListener("click", () => {
     });
 
 
+const btn2 = document.querySelector(#btn2);
 btn2.addEventListener("click",() => {
-    console.log("click2")
-;    const square = div.querySelector(#secondZone);
+    console.log("click2");
+    const square = div.querySelector(#secondZone);
     console.log(square);
-    square.createElement("div",id="tile");
+    square.createElement("div", id="tile");
     const tile = square.querySelector(#tile);
     console.log(tile);
     tile.add(<p>tuile ici</p>);
