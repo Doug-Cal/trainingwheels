ad7e57380b4820a023fbc2d0aa04d261c62dfd07
Doug-Cal <112691904+Doug-Cal@users.noreply.github.com>
2022-09-13 11:06:33 +0200

    go

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ btn1.addEventListener("click", () => {
     btn1.classList.add("buttonMouseOver");
     btn1.style.padding = "0px";
 });
+btn1.addEventListener("mouseover", () => {
+    btn1.style.background = "black";
+});
