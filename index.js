b936b634fc19fa3d1406e2065d79597259ea5426
Doug-Cal <112691904+Doug-Cal@users.noreply.github.com>
2022-09-16 16:17:33 +0200

    go

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 console.log("Le script est lancé!");
 let tileNumber = 0;
 
-const btn1 = document.getElementById('btn1');
-btn1.addEventListener("mouseover", () => {
-    btn1.style.color = 'red';});
+
 
 function addTile () {
     const tile = document.createElement("div");
@@ -18,16 +16,38 @@ function addTile () {
         const tileButton = document.createElement("button");
         tileButton.classList = '.headerButton';
         tileButton.id = 'nextButton';
-        console.log(tileButton);
-
         
         tile.appendChild(tileButton);
     // Attaching the new elements to the div
     const currentDiv = document.getElementById("secondZone");
-    currentDiv.appendChild(tile);}
+    currentDiv.appendChild(tile);
+}
+
+
+
+function step(){
+    const actionTitle = document.getElementById('action');
+        actionTitle.document.createTextNode(`Ingrédients et ustensiles`);
+    const descriptionText = document.getElementById('description');
+        let shopList = [['apples', 2], ['oranges', 1]];    
+        descriptionText.createTextNode(`${shopList}`);
+    const next = document.getElementById('nextStep');
+        next.addEventListener('click', () => {
+            console.log('next step when clicked')});
+    const counter = document.getElementById('stepCounter');
+        counter.createTextNode('2/2');
+                }
+
+
+
+
+
+const btn1 = document.getElementById('btn1');
+    btn1.addEventListener("mouseover", () => {
+    btn1.style.color = 'red';});
 
 const btn2 = document.getElementById('btn2');
-btn2.addEventListener("click",() => {
+    btn2.addEventListener("click",() => {
     if(tileNumber === 0){
     addTile();
     tileNumber++}});
--- objects.js
+++ objects.js
@@ -1,3 +1,10 @@
 const tileContent = {
     title:"Première recette !",
     no:1};
+
+
+const recipe = {
+    name:"",
+    stepNumber :"",
+    ingredients:[],
+}
