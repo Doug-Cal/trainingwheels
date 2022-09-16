e0aab0403afa37396568f5caa96796a4386f94f7
Doug-Cal <112691904+Doug-Cal@users.noreply.github.com>
2022-09-16 13:17:44 +0200

    test

--- index.js
+++ index.js
@@ -11,9 +11,17 @@ function addTile () {
         title:"Première recette !",
         no:1};
     const newContent = document.createTextNode(`#${tileContent.no} : ${tileContent.title} `);
-    tile.appendChild(newContent);
-    tile.innerHTML = '<h1> TEST <button class="headerButton" id="btn1btnNext">Suivant</button></h1>';
-    tile.classList = '.topbar';
+    
+    
+    /* ci-dessous instancier un bouton dans l'invocation elle-même */
+    const tileButton = document.createElement("button");
+    tileButton.classList = '.headerButton';
+   
+    /* tile.innerHTML = '<h1> TEST <button class="headerButton" id="btn1btnNext">Suivant</button></h1>'; */
+        /* FIN ZONE DE TRAVAIL */
+        tile.classList = '.topbar';
+        tile.appendChild(newContent);
+        tile.appendChild(tileButton);
     // Attaching the new elements to the div
     const currentDiv = document.getElementById("secondZone");
     currentDiv.appendChild(tile);}
