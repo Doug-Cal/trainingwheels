0f06e8fe9626a9ed52a775e6876a5b93b627668d
Doug-Cal <112691904+Doug-Cal@users.noreply.github.com>
2022-09-16 10:14:58 +0200

    go

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ function addTile () {
     // add the text node to the newly created div
     newDiv.appendChild(newContent);
     // add the newly created element and its content into the DOM
-    const currentDiv = document.getElementById("secondZone");
-    document.body.main.insertBefore(newDiv, currentDiv);}
+    //const currentDiv = document.getElementById("secondZone");
+    document.body.main.insertBefore(newDiv, null);}
     const btn2 = document.getElementById('btn2');
 btn2.addEventListener("click",() => {
     console.log("click2");
