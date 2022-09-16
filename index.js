e708362c44483ddd81bc5ccfc55f03e18ba9bd42
Doug-Cal <112691904+Doug-Cal@users.noreply.github.com>
2022-09-16 09:44:15 +0200

    fix casing getElementById

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ btn1.addEventListener("mouseover", () => {
 const btn2 = document.getElementById('btn2');
 btn2.addEventListener("click",() => {
     console.log("click2");
-    const square = document.getElementbyId('secondZone');
+    const square = document.getElementById('secondZone');
     console.log(square);
     square.createElement("div", id="tile", classList ="tile");
-    const tile = square.getElementbyId("#tile");
+    const tile = square.getElementById("#tile");
     console.log(tile);
     //tile.add('<p>tuile ici</p>');
         tile.addEventListener('mouseover',() => {
